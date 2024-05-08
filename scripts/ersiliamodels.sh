50dcb02c3f49d1e9a9fde25df01e9aa3337666a3
GemmaTuron <gemma@ersilia.io>
2024-05-08 15:00:38 +0200

    mmv results

--- scripts/ersiliamodels.sh
+++ scripts/ersiliamodels.sh
@@ -3,27 +3,32 @@ ersilia -v run -i "../data/mmv_malariabox.csv" -o "../data/eos3804_mmv_malariabo
 ersilia -v run -i "../data/mmv_pathogenbox.csv" -o "../data/eos3804_mmv_pathogenbox.csv"
 ersilia -v run -i "../data/mmv_pandemicbox.csv" -o "../data/eos3804_mmv_pandemicbox.csv"
 ersilia -v run -i "../data/mmv_ghbox.csv" -o "../data/eos3804_mmv_ghbox.csv"
+ersilia close
 
 ersilia serve eos9ei3
 ersilia -v run -i "../data/mmv_malariabox.csv" -o "../data/eos9ei3_mmv_malariabox.csv"
 ersilia -v run -i "../data/mmv_pathogenbox.csv" -o "../data/eos9ei3_mmv_pathogenbox.csv"
 ersilia -v run -i "../data/mmv_pandemicbox.csv" -o "../data/eos9ei3_mmv_pandemicbox.csv"
 ersilia -v run -i "../data/mmv_ghbox.csv" -o "../data/eos9ei3_mmv_ghbox.csv"
+ersilia close
 
 ersilia serve eos81ew
 ersilia -v run -i "../data/mmv_malariabox.csv" -o "../data/eos81ew_mmv_malariabox.csv"
 ersilia -v run -i "../data/mmv_pathogenbox.csv" -o "../data/eos81ew_mmv_pathogenbox.csv"
 ersilia -v run -i "../data/mmv_pandemicbox.csv" -o "../data/eos81ew_mmv_pandemicbox.csv"
 ersilia -v run -i "../data/mmv_ghbox.csv" -o "../data/eos81ew_mmv_ghbox.csv"
+ersilia close
 
 ersilia serve eos5jz9
 ersilia -v run -i "../data/mmv_malariabox.csv" -o "../data/eos5jz9_mmv_malariabox.csv"
 ersilia -v run -i "../data/mmv_pathogenbox.csv" -o "../data/eos5jz9_mmv_pathogenbox.csv"
 ersilia -v run -i "../data/mmv_pandemicbox.csv" -o "../data/eos5jz9_mmv_pandemicbox.csv"
 ersilia -v run -i "../data/mmv_ghbox.csv" -o "../data/eos5jz9_mmv_ghbox.csv"
+ersilia close
 
 ersilia serve eos2hbd
 ersilia -v run -i "../data/mmv_malariabox.csv" -o "../data/eos2hbd_mmv_malariabox.csv"
 ersilia -v run -i "../data/mmv_pathogenbox.csv" -o "../data/eos2hbd_mmv_pathogenbox.csv"
 ersilia -v run -i "../data/mmv_pandemicbox.csv" -o "../data/eos2hbd_mmv_pandemicbox.csv"
 ersilia -v run -i "../data/mmv_ghbox.csv" -o "../data/eos2hbd_mmv_ghbox.csv"
+ersilia close
