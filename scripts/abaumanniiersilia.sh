b81e633e94cef9c77670509bf0f2a3c77bc17278
GemmaTuron <gemma@ersilia.io>
2024-05-10 16:49:19 +0200

    abaumannii herg preds

--- scripts/abaumanniiersilia.sh
+++ scripts/abaumanniiersilia.sh
@@ -1,20 +1,14 @@
-ersilia serve eos81ew
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_0.csv" -o "../data/abaumannii/eos81ew_abaumannii_subset_0.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_1.csv" -o "../data/abaumannii/eos81ew_abaumannii_subset_1.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_2.csv" -o "../data/abaumannii/eos81ew_abaumannii_subset_2.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_3.csv" -o "../data/abaumannii/eos81ew_abaumannii_subset_3.csv"
+
+ersilia serve eos43at
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_0.csv" -o "../data/abaumannii/eos43at_abaumannii_subset_0.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_1.csv" -o "../data/abaumannii/eos43at_abaumannii_subset_1.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_2.csv" -o "../data/abaumannii/eos43at_abaumannii_subset_2.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_3.csv" -o "../data/abaumannii/eos43at_abaumannii_subset_3.csv"
 ersilia close
 
-ersilia serve eos5jz9
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_0.csv" -o "../data/abaumannii/eos5jz9_abaumannii_subset_0.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_1.csv" -o "../data/abaumannii/eos5jz9_abaumannii_subset_1.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_2.csv" -o "../data/abaumannii/eos5jz9_abaumannii_subset_2.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_3.csv" -o "../data/abaumannii/eos5jz9_abaumannii_subset_3.csv"
-ersilia close
-
-ersilia serve eos2hbd
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_0.csv" -o "../data/abaumannii/eos2hbd_abaumannii_subset_0.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_1.csv" -o "../data/abaumannii/eos2hbd_abaumannii_subset_1.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_2.csv" -o "../data/abaumannii/eos2hbd_abaumannii_subset_2.csv"
-ersilia -v run -i "../data/abaumannii/abaumannii_subset_3.csv" -o "../data/abaumannii/eos2hbd_abaumannii_subset_3.csv"
+ersilia serve eos9ei3
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_0.csv" -o "../data/abaumannii/eos9ei3_abaumannii_subset_0.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_1.csv" -o "../data/abaumannii/eos9ei3_abaumannii_subset_1.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_2.csv" -o "../data/abaumannii/eos9ei3_abaumannii_subset_2.csv"
+ersilia -v run -i "../data/abaumannii/abaumannii_subset_3.csv" -o "../data/abaumannii/eos9ei3_abaumannii_subset_3.csv"
 ersilia close
